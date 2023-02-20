564630a8c83aa971148b2493767e5094eb83c602
Mikołaj Mrózek <31847808+reqill@users.noreply.github.com>
2023-02-20 12:06:57 +0100

    feat(chess): reset game, flip board, move indicator (#8)
    * feat(chess): advanced movement rules and sound
    
    * feat(chess): board flip and side to move indicator
    
    * feat(chess): QoL

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,12 @@
-<script>
-	import ChessPiece from '$lib/components/chess/pieces/ChessPiece.svelte';
-</script>
-
 <svelte:head>
 	<title>frozone-chess</title>
 	<meta name="description" content="Chess online platform" />
 </svelte:head>
 
-<div class="flex pt-5">
-	<ChessPiece piece="pawn" side="white" />
-	<ChessPiece piece="king" side="white" />
-	<ChessPiece piece="queen" side="white" />
-	<ChessPiece piece="bishop" side="white" />
-	<ChessPiece piece="rook" side="white" />
-	<ChessPiece piece="knight" side="white" />
-</div>
-<div class="flex pb-5">
-	<ChessPiece piece="pawn" side="black" />
-	<ChessPiece piece="king" side="black" />
-	<ChessPiece piece="queen" side="black" />
-	<ChessPiece piece="bishop" side="black" />
-	<ChessPiece piece="rook" side="black" />
-	<ChessPiece piece="knight" side="black" />
+<div class="flex w-full">
+	<a
+		href="/play"
+		class="m-auto rounded-xl bg-blue-400 py-4 px-12 text-4xl font-bold text-gray-100 shadow-md transition-all hover:px-14 hover:no-underline hover:shadow-xl"
+		>Play</a
+	>
 </div>
