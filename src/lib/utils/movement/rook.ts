b902d72d1bc3481248898f14c69fb5ae57d143cd
Mikołaj Mrózek <31847808+reqill@users.noreply.github.com>
2023-02-22 15:25:16 +0100

    refactor(utils): piece movement

---
+++ src/lib/utils/movement/rook.ts
@@ -0,0 +1,11 @@
+import { MOVE_INDEX_CHANGE } from "$lib/constants/movement.constants";
+import type { PieceType, SquareInfoType } from "$lib/types/chess.types";
+import type { PositionStoreValueType } from "$lib/types/store.types";
+
+
+export const rook = (positions: PositionStoreValueType, piece: PieceType) => {
+    const possibleMoves: SquareInfoType[] = [];
+    const attackMoves: SquareInfoType[] = [];
+
+    return { possibleMoves, attackMoves };
+}
