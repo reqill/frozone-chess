2621a35a77500134ab6cecbba53b940f3a739a60
Mikołaj Mrózek <mikolaj.mrozek@imgarena.com>
2023-03-03 12:15:27 +0100

    feat: extend button styling

--- src/lib/components/chess/buttons/PrimaryButton/PrimaryButton.svelte
+++ src/lib/components/chess/buttons/PrimaryButton/PrimaryButton.svelte
@@ -16,20 +16,30 @@
 
 		dispatch('click', e);
 	};
+
+	$: buttonStyles = disabled
+		? active
+			? 'bg-primary-disabled text-app-white text-opacity-90'
+			: 'bg-disabled-bg text-disabled-text'
+		: active
+		? 'bg-primary text-app-white hover:bg-primary-light active:translate-y-1'
+		: 'bg-app-white text-app-black hover:bg-primary hover:text-app-white active:translate-y-1';
+
+	$: shadowStyles = disabled
+		? active
+			? 'bg-primary-shadow-disabled'
+			: 'bg-btn-shadow'
+		: active
+		? 'bg-primary-shadow group-hover:bg-primary-shadow-light group-active:translate-y-0 group-active:-z-10'
+		: 'bg-btn-shadow group-hover:bg-primary-shadow group-active:translate-y-0 group-active:-z-10	';
 </script>
 
 <!-- horizontal -->
 <button
 	on:click={handleClick}
 	{disabled}
-	class="my-2 mx-1 rounded-[.2rem] py-3 px-7 font-test text-2xl font-bold {!fullWidth &&
-		'self-center'} {disabled
-		? active
-			? 'bg-primary-shadow-disabled text-app-white text-opacity-90'
-			: 'bg-disabled-bg text-disabled-text'
-		: active
-		? 'bg-primary text-app-white'
-		: 'bg-app-white text-app-black'} transition-all ease-in-out"
+	class="group relative my-2 mx-1 py-3 px-7 font-test text-2xl font-bold {!fullWidth &&
+		'self-center'} {buttonStyles} btn-base group"
 >
 	{#if icon && iconPlacement === 'left'}
 		<span class="p-btn-icon">{icon}</span>
@@ -38,4 +48,11 @@
 	{#if icon && iconPlacement === 'right'}
 		<span class="p-btn-icon">{icon}</span>
 	{/if}
+	<span class="absolute inset-0 -z-10 {shadowStyles} btn-base translate-y-1" />
 </button>
+
+<style lang="postcss">
+	.btn-base {
+		@apply rounded-[.2rem] transition-all ease-in-out;
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,13 +11,12 @@ module.exports = {
 		extend: {
 			colors: {
 				primary: '#00B9FF',
-				secondary: '#ff3e00',
 				'primary-light': '#00C2FF',
 				'primary-disabled': '#94BECE',
 				'primary-shadow': '#0183B4',
 				'primary-shadow-light': '#00A0D0',
 				'primary-shadow-disabled': '#7A9FAC',
-				'secondary-shadow': '#BBBBBB',
+				'btn-shadow': '#BBBBBB',
 				'app-white': '#ffffff',
 				'app-bg': '#F3F3F3',
 				'app-black': '#000000',
@@ -26,7 +25,7 @@ module.exports = {
 			},
 			fontFamily: {
 				test: ['Ebrima', 'sans-serif'],
-			},
+			}
 		},
 	},
 	plugins: [],
