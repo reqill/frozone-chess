b6268e3b3c35962a6e03d6068c80c84702b6d29f
Mikołaj Mrózek <31847808+reqill@users.noreply.github.com>
2023-03-03 10:25:49 +0100

    feat: new styling type

--- src/lib/components/chess/buttons/PrimaryButton/PrimaryButton.svelte
+++ src/lib/components/chess/buttons/PrimaryButton/PrimaryButton.svelte
@@ -19,7 +19,18 @@
 </script>
 
 <!-- horizontal -->
-<button on:click={handleClick} {disabled} data-fullwidth={fullWidth} data-active={active}>
+<button
+	on:click={handleClick}
+	{disabled}
+	class="my-2 mx-1 rounded-[.2rem] py-3 px-7 font-test text-2xl font-bold {!fullWidth &&
+		'self-center'} {disabled
+		? active
+			? 'bg-primary-shadow-disabled text-app-white text-opacity-90'
+			: 'bg-disabled-bg text-disabled-text'
+		: active
+		? 'bg-primary text-app-white'
+		: 'bg-app-white text-app-black'} transition-all ease-in-out"
+>
 	{#if icon && iconPlacement === 'left'}
 		<span class="p-btn-icon">{icon}</span>
 	{/if}
@@ -28,66 +39,3 @@
 		<span class="p-btn-icon">{icon}</span>
 	{/if}
 </button>
-
-<style lang="postcss">
-	button {
-		@apply relative my-2 mx-1 rounded-[.2rem] bg-app-white py-3 px-7 text-app-black transition-all ease-in-out;
-	}
-
-	button:not([data-fullwidth='true']) {
-		@apply self-center;
-	}
-
-	button::after {
-		content: '';
-		@apply absolute inset-0 -z-[1] translate-y-1 rounded-[.2rem] bg-secondary-shadow transition-all ease-in-out;
-	}
-
-	button:not([disabled]):hover::after {
-		content: '';
-		@apply translate-y-1 bg-primary-shadow;
-	}
-
-	button[data-active='true'] {
-		@apply bg-primary text-app-white;
-	}
-
-	button[data-active='true']::after {
-		content: '';
-		@apply translate-y-1 bg-primary-shadow;
-	}
-
-	button[data-active='true']:not([disabled]):hover {
-		@apply bg-primary-light text-app-white;
-	}
-
-	button[data-active='true']:not([disabled]):hover::after {
-		content: '';
-		@apply translate-y-1 bg-primary-shadow-light;
-	}
-
-	button:disabled[data-active='true'] {
-		@apply bg-primary-disabled text-app-white;
-	}
-
-	button:disabled[data-active='true']::after {
-		content: '';
-		@apply translate-y-1 bg-primary-shadow-disabled;
-	}
-
-	button:disabled {
-		@apply bg-disabled-bg text-disabled-text;
-	}
-
-	button:not([disabled]):hover {
-		@apply bg-primary text-app-white;
-	}
-
-	.p-btn-icon {
-		@apply inline-block fill-app-black;
-	}
-
-	.p-btn-label {
-		@apply font-test text-2xl font-bold;
-	}
-</style>
