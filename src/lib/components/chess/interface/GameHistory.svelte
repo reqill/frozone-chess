d90d19b506110b45a661d1bd16a55445826d6c19
Mikołaj Mrózek <31847808+reqill@users.noreply.github.com>
2023-03-17 15:45:12 +0100

    feat(chess): styled history (#77)

--- src/lib/components/chess/interface/GameHistory.svelte
+++ src/lib/components/chess/interface/GameHistory.svelte
@@ -11,32 +11,55 @@
 </script>
 
 <div
-	class="h-full overflow-x-auto"
+	class="h-full overflow-x-auto pt-1"
 	id="history-panel"
 	style="max-height: {mountHeight || 'auto'}{mountHeight ? 'px' : ''};"
 >
 	<table>
-		<thead>
-			<tr>
-				<th> Move </th>
-				<th> White </th>
-				<th> Black </th>
-			</tr>
-		</thead>
 		<tbody>
 			{#each historyArray as _, i}
 				<tr>
 					<td>
-						{i + 1}.
+						<p>{i + 1}.</p>
 					</td>
 					<td>
-						{$history.moves.get(i * 2)}
+						<p>{$history.moves.get(i * 2)}</p>
 					</td>
 					<td>
-						{$history.moves.get(i * 2 + 1) || ''}
+						{#if i * 2 + 1 < $history.moves.size}
+							<p>{$history.moves.get(i * 2 + 1) || ''}</p>
+						{/if}
 					</td>
 				</tr>
 			{/each}
 		</tbody>
 	</table>
 </div>
+
+<style lang="postcss">
+	table {
+		@apply mt-[.15rem];
+	}
+	tr {
+		@apply flex flex-row flex-nowrap;
+	}
+
+	td:first-child {
+		@apply w-[3ch] px-1 pt-[.3rem] text-right text-xs;
+	}
+
+	td:nth-child(2) {
+		@apply flex w-[9ch] flex-row justify-start px-1 text-center;
+	}
+	td:nth-child(3) {
+		@apply flex w-[9ch] flex-row justify-start px-1 text-center;
+	}
+
+	td:nth-child(2) > p {
+		@apply rounded-[.25rem] bg-gray-100 px-2 py-[.1rem] text-center text-sm;
+	}
+
+	td:nth-child(3) > p {
+		@apply rounded-[.25rem] bg-app-black/75 px-2 py-[.1rem] text-center text-sm  text-app-white;
+	}
+</style>
--- src/lib/components/chess/interface/InfoBox.svelte
+++ src/lib/components/chess/interface/InfoBox.svelte
@@ -3,12 +3,12 @@
 	export let label: undefined | string = undefined;
 </script>
 
-<div style="height: calc(100% - {label ? '.5rem' : '0'});" class={width}>
+<div style="height: calc(100% - {label ? '.6rem' : '0'});" class={width}>
 	{#if label}
 		<h5 class="pl-1 text-[.9rem] text-app-black/95 first-letter:font-test">{label}</h5>
 	{/if}
 	<div
-		style="height: calc(100% - {label ? '.5rem' : '0'});"
+		style="height: calc(100% - {label ? '.6rem' : '0'});"
 		class="rounded-md border-2 border-gray-500/10 bg-app-white"
 	>
 		<slot />
